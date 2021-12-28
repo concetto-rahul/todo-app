eb8ddcd0ed86768aa8bfbbecf0c967fa0d47fc30
Bhavin D <bhavin.doshi@concettolabs.com>
2021-12-28 19:55:38 +0530

    work on firebase functions and connect function

--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -1,56 +1,137 @@
-import { ReactElement,FC,useContext,useState } from "react";
-import { TaskContext } from '../../context/task';
+import { ReactElement, FC, useContext, useState, useEffect } from "react";
+import { TaskContext } from "../../context/task";
 import Page from "../../components/Page";
 
-import { Tooltip,Fab } from "@material-ui/core";
-import AddIcon from '@material-ui/icons/Add';
-import SyncIcon from '@material-ui/icons/Sync';
+import { Tooltip, Fab } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import SyncIcon from "@material-ui/icons/Sync";
+
+import { getFunctions, httpsCallable } from "firebase/functions";
+import { useFirebase } from "../../firebase-client/useInitFirebase";
 
 import TaskForm from "./taskForm";
 import TaskList from "./taskList";
 
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     iconButton: {
-        position:"relative",
+      position: "relative",
     },
     iconButton2: {
-        position: "absolute",
-        right: "0",
+      position: "absolute",
+      right: "0",
     },
     syncIcon: {
       marginRight: theme.spacing(1),
     },
-  }),
+  })
 );
 
-const TodoApp:FC<any>=() : ReactElement=>{
-    const { state, dispatch } = useContext(TaskContext);
-    const [openTaskForm, setOpenTaskForm] = useState(false);
-    const classes = useStyles();
+const TodoApp: FC<any> = (): ReactElement => {
+  const app = useFirebase();
+  const functions = getFunctions(app);
+  const queryParams = new URLSearchParams(window.location.search);
+  const redirectURLCode = queryParams.get("code");
+  const { state, dispatch } = useContext(TaskContext);
+  const [openTaskForm, setOpenTaskForm] = useState(false);
+  const [syncingListData, setSyncingListData] = useState(false);
+  const classes = useStyles();
 
-    const handleClickOpenTaskForm = () => {
-        setOpenTaskForm(true);
-    };
-    return(
-        <Page title="Task list" description="you can view your task list" key="todo,list,add,view,abcgfd,sgdgdg">
-            <TaskForm openTaskForm={openTaskForm} setOpenTaskForm={setOpenTaskForm} dispatch={dispatch}/>
-            <div className={classes.iconButton}>
-                <Tooltip title="Add your new task" aria-label="add">
-                    <Fab color="primary" onClick={handleClickOpenTaskForm}>
-                        <AddIcon />
-                    </Fab>
-                </Tooltip>
-                <Tooltip title="sync to gmail account " aria-label="add">
-                    <Fab  className={classes.iconButton2} color="secondary" variant="extended" >
-                        <SyncIcon className={classes.syncIcon}  />  Sync To Gmail Account
-                    </Fab>
-                </Tooltip>
-            </div>
-            <TaskList tasks={state?.list} />
-        </Page>
-    )
-}
+  useEffect(() => {
+    async function updateListData() {
+      setSyncingListData(true);
+      console.log("redirectURLCode", redirectURLCode);
+      const uploadFile = await httpsCallable(functions, "uploadFile");
+      await uploadFile({
+        code: redirectURLCode,
+        listData:[
+            {
+                "id": 1640699479291,
+                "title": "Create Your New Task One",
+                "description": "Create Your New Task One Create Your New Task One Create Your New Task One Create Your New Task One Create Your New Task OneCreate Your New Task One Create Your New Task One",
+                "createdAt": "2021-12-28T13:51:19.291Z",
+                "updatedAt": "2021-12-28T13:51:19.291Z",
+                "status": "pending"
+            },
+            {
+                "id": 1640699490360,
+                "title": "Create Your New Task Two",
+                "description": "Create Your New Task Two Create Your New Task Two Create Your New Task Two Create Your New Task Two Create Your New Task Two Create Your New Task Two",
+                "createdAt": "2021-12-28T13:51:30.360Z",
+                "updatedAt": "2021-12-28T13:51:30.360Z",
+                "status": "pending"
+            },
+            {
+                "id": 1640699501020,
+                "title": "Create Your New Task Three",
+                "description": "Create Your New Task Three Create Your New Task Three Create Your New Task Three Create Your New Task Three Create Your New Task Three Create Your New Task Three",
+                "createdAt": "2021-12-28T13:51:41.020Z",
+                "updatedAt": "2021-12-28T13:51:41.020Z",
+                "status": "pending"
+            }
+        ]
+      })
+        .then((res: any) => {
+          console.log("uploadFile res",res?.data);
+        })
+        .catch((err) => console.log("uploadFile Error", err));
+    }
+    redirectURLCode && updateListData();
+  }, [redirectURLCode]);
+
+  const handleClickOpenTaskForm = () => {
+    setOpenTaskForm(true);
+  };
+
+  const handleAuthURL = async () => {
+    if (!syncingListData) {
+      const generateAuthUrl = await httpsCallable(functions, "generateAuthUrl");
+      await generateAuthUrl()
+        .then((res: any) => {
+          console.log(res?.data?.authUrl);
+          if (res?.data?.authUrl) {
+            window.location.href = `${res?.data?.authUrl}`;
+          }
+        })
+        .catch((err) => console.log("Error", err));
+    } else {
+      alert("Please wait data syncingp0 is in progress...");
+    }
+  };
+
+  return (
+    <Page
+      title="Task list"
+      description="you can view your task list"
+      key="todo,list,add,view,abcgfd,sgdgdg"
+    >
+      <TaskForm
+        openTaskForm={openTaskForm}
+        setOpenTaskForm={setOpenTaskForm}
+        dispatch={dispatch}
+      />
+      <div className={classes.iconButton}>
+        <Tooltip title="Add your new task" aria-label="add">
+          <Fab color="primary" onClick={handleClickOpenTaskForm}>
+            <AddIcon />
+          </Fab>
+        </Tooltip>
+        <Tooltip title="sync to gmail account " aria-label="add">
+          <Fab
+            className={classes.iconButton2}
+            color="secondary"
+            variant="extended"
+            onClick={handleAuthURL}
+          >
+            <SyncIcon className={classes.syncIcon} /> Sync To Gmail Account
+          </Fab>
+        </Tooltip>
+      </div>
+      <TaskList tasks={state?.list} />
+    </Page>
+  );
+};
 
 export default TodoApp;
